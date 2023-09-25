939a8c54f9fcd9e9c0b0d51efa3a805d64a24d7b
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2023-09-25 16:04:33 -0400

    Add project files.

---
+++ Models/Estados.cs
@@ -0,0 +1,14 @@
+﻿using PizzeriaPlace.BLL;
+
+namespace PizzeriaPlace.Models
+{
+    public class Estados
+    {
+        public MenuLocal Menu { get; set; }  = new MenuLocal();
+        public CestaDCompras cestaDCompras { get; } = new CestaDCompras();
+        public InterfazUsuario Interfaz { get; set; } = new InterfazUsuario();
+        public decimal PrecioTotal => cestaDCompras.Pedidos.Sum(Id => Menu.ObtenerPizzas(Id)!.Precio);
+
+
+    }
+}
